f87413a9d595606a24aa4d7592a6a3fdb65e20b8
William Koller <wkoller25@gmail.com>
2020-08-22 23:52:50 -0300

    feat: ensure AuthUseCase throws if email is provided

---
+++ src/domain/usecases/auth-usecase.spec.js
@@ -0,0 +1,13 @@
+class AuthUseCase {
+  async auth (email) {
+    if (!email) throw new Error()
+  }
+}
+
+describe('Auth UseCase', () => {
+  test('Should throw if no email is provided', async () => {
+    const sut = new AuthUseCase()
+    const promise = sut.auth()
+    expect(promise).rejects.toThrow()
+  })
+})
